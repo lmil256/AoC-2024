5b20ca113df31042a27bdff6b737fbae26240843
Laurence Miller <52897574+lmil256@users.noreply.github.com>
2024-12-10 10:16:27 -0500

    Day 7 Part 2

---
+++ day7/part2.py
@@ -0,0 +1,23 @@
+def main():
+    result = 0
+    with open('input.txt') as infile:
+        for line in infile:
+            answer, terms = line.split(': ')
+            answer = int(answer)
+            terms = [int(x) for x in terms.split(' ')]
+            if find_solution(answer, terms):
+                result += answer
+
+    print(result)
+
+def find_solution(answer, terms):
+    if len(terms) == 2:
+        return terms[0]+terms[1] == answer \
+                or terms[0]*terms[1] == answer \
+                or int(str(terms[0])+str(terms[1])) == answer
+    return find_solution(answer, [terms[0]+terms[1]]+terms[2:]) \
+            or find_solution(answer, [terms[0]*terms[1]]+terms[2:]) \
+            or find_solution(answer, \
+                    [int(str(terms[0])+str(terms[1]))]+terms[2:])
+
+main()
